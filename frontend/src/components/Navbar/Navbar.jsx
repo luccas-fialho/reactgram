9af771415b5b1b6a5c0d25cfab9095e5567c92ea
Luccas Fialho <luccas.flh@gmail.com>
2025-01-21 13:33:46 -0300

    :lipstick: feat: Navbar and Register styles

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav id="nav">
       <Link to="/">Reactgram</Link>
-      <form>
+      <form id="search-form">
         <BsSearch />
         <input type="text" />
       </form>
--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Auth.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div id="register">
+      <h2>Reactgram</h2>
+      <p>Sign up so you can see your friends posts!</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm password" />
+        <input type="submit" value="Sign up" />
+      </form>
+      <p>
+        Already has an account? <Link to="/login">Sign in</Link>
+      </p>
     </div>
   );
 };
