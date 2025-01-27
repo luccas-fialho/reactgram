146a09a1c056df132b17c99ee558fdbe3dd20d66
Luccas Fialho <luccas.flh@gmail.com>
2025-01-27 11:31:31 -0300

    :sparkles: feat: Login func and form

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Auth.css";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { login, reset } from "../../slices/authSlice";
+import Message from "../../components/Message/Message";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const { loading, error } = useSelector((state) => state.auth);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    dispatch(login(data));
+  };
+
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div id="login">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <p>Log in to see users posts!</p>
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        {!loading && <input type="submit" value="Log in" />}
+        {loading && <input type="submit" value="Loading..." disabled />}
+        {error && <Message msg={error} type="error" />}
+      </form>
+      <p>
+        Don't have an account? <Link to="/register">Sign up</Link>
+      </p>
     </div>
   );
 };
--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -20,9 +20,28 @@ const logout = () => {
   localStorage.removeItem("user");
 };
 
+const login = async (data) => {
+  try {
+    const config = requestConfig("POST", data);
+    const res = await fetch(api + "/users/login", config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    if (res._id) {
+      localStorage.setItem("user", JSON.stringify(res));
+      return res;
+    }
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const authService = {
   register,
   logout,
+  login,
 };
 
 export default authService;
