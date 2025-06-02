9837a9e086487e68fc14dff171273623a443ad2c
Luccas Fialho <luccas.flh@gmail.com>
2025-06-02 10:35:51 -0300

    feat: environment variables

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,5 +1,6 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+export const api = process.env.VITE_API_URL || "http://localhost:5000/api";
+export const uploads =
+  process.env.VITE_UPLOADS || "http://localhost:5000/uploads";
 
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
